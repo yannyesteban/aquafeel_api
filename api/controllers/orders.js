19c2106b99a8ce109fecbd24c9096f92b096fe40
Yanny Nuñez <yannyesteban@hotmail.com>
2024-06-27 11:20:19 -0400

    pdf order added

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -238,27 +238,45 @@ module.exports.pdf1 = async (req, res, next) => {
 };
 
 module.exports.pdf = async (req, res, next) => {
-	const text1 =
+	const AquafeelSolutions =
 		"Aquafeel Solutions propone proveer todos los produtos, materiales y mano de obra para instalar lo especificado a continuacion y el comprador(es) ordena y compra los produtos, materiales y mano de obra e instalacion de lo especificado a continuacion:";
 
-	const text2 =
+	const THIS_CONTRACT_IS =
 		"THIS CONTRACT IS VALID ONLY UPON SIGNED APROVAL BY EMPLOYED MANAGEMENT PRESONNEL OF AQUAFEEL SOLUTIONS. SEE TERMS AND CONDITIONS ON REVERSE SIDE. A RESTOCKING FEE OF 20%. WILL BE CHARGE FOR TRANSACTIONS THAT ARE CANCELLED AFTER THREE DAY WATING PERIOD.";
-	const text3 =
+	const TEXT_230_Capcom =
 		"230 Capcom Ave Ste. 103, Wake Forest NC 27587 - Ph (919) 790-5475 - Fax (919) 790-5476 \nwww.aquafeelmaryland.com - info@aquafeelvirginia.com";
 
-	const text4 = "Upon signing, you acknowledge you agree to the terms and conditions of this contract. You agreed to all payments and charges and to the sales price appearing bellow."	
-	const text5 = "DO NOT SIGNED THIS CONTRACT UNTILL YOU HAVE READ IT ALL OF THE BLANK SPACES ARE COMPLETED. YOU HAVE THE RIGHT TO RECEIVE A COMPLETE COPY OF THIS CONTRACT. YOU HAVE THE RIGHT TO PREPAY FULL AMOUNT AT ANY TIME AND TO BE NOTIFIED OF THE FULL AMOUNT DUE."
-	
+	const UPON_SIGNING = "Upon signing, you acknowledge you agree to the terms and conditions of this contract. You agreed to all payments and charges and to the sales price appearing bellow."
+	const DO_NOT_SIGNED = "DO NOT SIGNED THIS CONTRACT UNTILL YOU HAVE READ IT ALL OF THE BLANK SPACES ARE COMPLETED. YOU HAVE THE RIGHT TO RECEIVE A COMPLETE COPY OF THIS CONTRACT. YOU HAVE THE RIGHT TO PREPAY FULL AMOUNT AT ANY TIME AND TO BE NOTIFIED OF THE FULL AMOUNT DUE."
+	const termsAndConditions = `OTHER TERMS AND CONDITIONS
+STATE LAW REQUIRES THAT ANYONE WHO CONTRACTS TO DO CONSTRUCTION WORK TO BE LICENSED BY THE CONTRACTORS STATE LICENSE BOARD IN THE LICENSE CATEGORY IN WHICH THE CONTRACTOR IS GOING TO BE WORKING- IF THE TOTAL PRICE OF THE JOB IS $500 OR MORE (INCLUDING LABOR AND MATERIALS). LICENSED CONTRACTORS ARE REGULATED BY LAWS DESIGNED TO PROTECT THE PUBLIC. IF YOU CONTRACT WITH SOMEONE WHO DOES NOT HAVE A LICENSE, THE CONTRACTORS STATE LICENSE BOARD MAY BE UNABLE TO ASSIST YOU WITH A COMPLAINT. YOUR ONLY REMEDY AGAINST UNLICENSED CONTRACTOR MAY BE IN CIVIL COURT AND YOU MAY BE LIABLE FOR DAMAGES ARISING OUT OF ANY INJURIES TO THE CONTRACTOR OR HIS OR HER EMPLOYEES. YOU MAY CONTACT THE CONTRACTORS STATE LICENSE BOARD TO FIND OUT IF THIS CONTRACTOR HAS A VALID LICENSE. THE BOARD HAS COMPLETE INFORMATION ON THE HISTORY OF LICENSED CONTRACTORS, INCLUDING ANY POSSIBLE SUSPENSIONS, REVOCATIONS, JUDGMENTS, AND CITATIONS. SEARCH IN THE WHITE PAGES FOR CONTRACTORS STATE LICENSE BOARD OFFICE NEAREST TO YOU.
+1. The tittle to the equipment and materials covered in this Contract shall remain the legal property of Aquafeel Solutions, until the equipment and materials are paid in full. You acknowledge that you are giving a security interest in the goods purchased. The Buyer(s) hereby agrees that there is no written agreement or verbal understanding of any kind or nature, with Aquafeel Solutions, or any of its representatives, whereby this Contract, or any part of it is be altered, modified, or varied in any manner whatsoever from the conditions herein. The terms and conditions of this Contract are complete and exclusive statement of the agreements between the parties, constitute the entire agreement, and supersede and cancel all prior or contemporaneous negotiations, statements, and representations. There are no representations, inducements, promises, or agreements, oral or otherwise, with reference to this sale other than expressly set forth herein. If it is not in writing, and approved by employed management personnel at Aquafeel Solutions, it will not be honored.
+I have received a copy of this document:
+Buyer’s Signature: _________________________ Buyer’s Signature: ____________________________
+2. Aquafeel Solutions, agrees to start and diligently pursue work through to completion, but shall not be responsible for delays for any of the following reasons: Funding of loans, acts of neglect or omissions of the Buyer(s) or the Buyer(s) agent, acts of God, stormy or inclement weather, extra work ordered by the Buyer(s), acts of Public Enemy, riots or civil commotion, failure of Buyer(s) to make your payment(s) when due, or for acts of independent contractors, or Holidays, or other causes beyond Aquafeel Solutions' control. Buyer(s) shall grant free access to work areas for workers, equipment, and vehicles. Aquafeel Solutions' workers shall not be responsible for keeping gates closed for animals and children.
+3. This contract shall be construed in accordance with and governed by the laws of the State in which this Contract is signed. If any provision(s) of this contract shall be invalid for any reason, such invalidity shall be confined to the requirements of applicable law and shall not affect the remainder hereof, which shall continue in full force and effect or constitute a binding agreement between parties.
+4. In the even a dispute relating to this Contract resulted in litigation between the parties, concerning the work hereunder or any event related thereto, the party prevailing in such dispute shall be entitled to reasonable attorney's fees and cost.
+5. All work will be done according to the approved standards in the industry. This does not included correction of defects in existing plumbing or other inadequate building conditions.
+6. Financing terms are subject to approval and verification by the financing institution. You hereby authorize the seller to obtain a credit report for the purpose of financing this Contract. Additional financing terms and truth-in-lending information will be provided by the finance company. In the event Buyer(s) is denied credit financing Contracts, upon demand, from any other financing institutions, companies, corporations, or banks, including but not limited to Security Agreements, Lien Contracts, or Assignments of Rent Contracts, repayments period, amount financed, and the APR (interest rate) are subject to change due to varying terms and conditions from secondary financing sources.
+7. Additional terms and conditions may be stated on a separate Addendum to Contract. If an Addendum is executed by the Buyer, it shall be incorporated herein and become a part of this Proposal-Work Order-Contract.
+NOTICE OF CANCELLATION
+Date of Transaction: ___/___/___ Cancellation Date: ___/___/___ (No later than midnight of this date)
+You, the Buyer(s), may cancel this transaction, without penalty or obligation, at any time prior to midnight of the third business day after the date of this transaction noted above (i.e., within three business days from the above date). If you cancel, any property traded in, any payments made by you will under the contract sale, and any negotiable instrument executed by you will be returned within 10 days following receipt by the seller of you cancellation notice, and any security interest arising out of the transaction will be cancelled. If you cancel, you must make available to the seller at your residence, in substantially as good condition as when received. If you fail to make the goods available to the seller, or if you agree to return the goods to the seller and fail to do so, then you remain liable for performance of all obligations under the Contract. If you do make the goods available to the seller and they are not picked up within 20 days following the date of this Notice of Cancellation, you may retain or dispose of the goods without any further obligation. To cancel this transaction, mail (if mailed, the copy should be post marked by the third day after the date of the transaction) or deliver a signed and dated copy of this Cancellation Notice or any other written notice, or send a telegram to:
+Aquafeel Solutions
+230 Capcom Ave Ste. 103, Wake Forest NC 27587 PH (919) 790-5475 • FAX (919) 790-5476
+I hereby cancel this transaction:
+Date:___/___/___ Buyer’sSignature:________________________ Date:___/___/___ Buyer’sSignature:________________________`;
+
+
 	const fontSize = 10;
-	const fontSize2 = 12;
 	const fontTitle = 9;
 	const bodyWidth = 585;
 	const cellFontSize = 8;
 	const noteFontSize = 7;
 	const startX = 15;
-		const startY = 25;
-		const columnWidths = [285, 150, 150];
-		const rowHeight = 22;
+	const startY = 25;
+	
+	const rowHeight = 22;
 
 	try {
 		let id = req.query.id;
@@ -268,7 +286,7 @@ module.exports.pdf = async (req, res, next) => {
 			return res.status(404).json({ message: "Order not found" });
 		}
 
-		
+
 		const doc = new PDFDocument({ size: "LETTER", autoFirstPage: false });
 
 		doc.addPage({
@@ -295,7 +313,7 @@ module.exports.pdf = async (req, res, next) => {
 		});
 		doc.moveDown();
 		doc.fontSize(fontSize).text("", startX, rowHeight * 4);
-	
+
 		doc.fontSize(fontTitle).text("PROPOSAL - WORK ORDER - CONTRACT", {
 			width: bodyWidth,
 			align: "center",
@@ -315,10 +333,10 @@ module.exports.pdf = async (req, res, next) => {
 					`PHONE 2: ${order.buyer2.phone}`,
 					`CEL: ${order.buyer2.cel}`,
 				]
-				
+
 			],
 		};
-		
+
 		doc.fontSize(cellFontSize);
 		drawTable(
 			doc,
@@ -332,7 +350,7 @@ module.exports.pdf = async (req, res, next) => {
 		const data7 = {
 			//headers: [],
 			rows: [
-				
+
 				[
 					`ADDRESS: ${order.address}`,
 					`CITY: ${order.city}`,
@@ -341,7 +359,7 @@ module.exports.pdf = async (req, res, next) => {
 				]
 			],
 		};
-		
+
 		doc.fontSize(cellFontSize);
 		drawTable(
 			doc,
@@ -357,7 +375,7 @@ module.exports.pdf = async (req, res, next) => {
 
 		doc.fontSize(noteFontSize).text("", startX, startY + rowHeight * 7.1);
 		//doc.fontSize(noteFontSize).text(text1, startX, startY + rowHeight * 7.1);
-		doc.fontSize(noteFontSize).text(text1, {
+		doc.fontSize(noteFontSize).text(AquafeelSolutions, {
 			width: 585,
 			align: "left",
 		});
@@ -375,7 +393,7 @@ module.exports.pdf = async (req, res, next) => {
 					`Brand: \n  ${order.system2.brand}`,
 					`Model: \n  ${order.system2.model}`,
 				],
-				[`Other \n  ${order.promotion}`, ``, ``],
+				//[`Other \n  ${order.promotion}`, ``, ``],
 			],
 		};
 
@@ -388,9 +406,31 @@ module.exports.pdf = async (req, res, next) => {
 			[195, 195, 195],
 			rowHeight
 		);
+
+		const data9 = {
+			//headers: [],
+			rows: [
+				
+				[`Other \n  ${order.promotion}`],
+			],
+		};
+
+		doc.fontSize(cellFontSize);
+		drawTable(
+			doc,
+			data9,
+			startX,
+			startY + rowHeight * 10,
+			[bodyWidth],
+			rowHeight
+		);
+
+
+
+		
 		//doc.moveDown();
 		//doc.fontSize(cellFontSize).text(text1, { align: "center" });
-		
+
 
 		doc.fontSize(cellFontSize).text("", startX, startY + rowHeight * 12);
 		doc.fontSize(fontTitle).text("INSTALLATION INSTRUCTIONS", {
@@ -437,16 +477,16 @@ module.exports.pdf = async (req, res, next) => {
 			rowHeight
 		);
 
-		
+
 		//doc.lineJoin("miter").rect(5, 10, 5, 5).stroke();
-		
+
 		doc.fontSize(fontTitle).text("", startX, rowHeight * 17);
 		doc.fontSize(fontTitle).text("Terms or Payment Methods".toUpperCase(), {
 			width: 585,
 			align: "center",
 		});
 
-		
+
 		const data21 = {
 			//headers: [],
 			rows: [
@@ -457,8 +497,8 @@ module.exports.pdf = async (req, res, next) => {
 			],
 		};
 
-		doc.moveDown();doc.moveDown();doc.moveDown();doc.moveDown();
-		doc.fontSize(noteFontSize).text("\n" + text4, {
+		doc.moveDown(); doc.moveDown(); doc.moveDown(); doc.moveDown();
+		doc.fontSize(noteFontSize).text("\n" + UPON_SIGNING, {
 			width: bodyWidth,
 			align: "justified",
 		});
@@ -533,7 +573,7 @@ module.exports.pdf = async (req, res, next) => {
 		);
 
 		doc.fontSize(cellFontSize).text("", startX, rowHeight * 27);
-		doc.fontSize(noteFontSize).text(text5.toUpperCase(), {
+		doc.fontSize(noteFontSize).text(DO_NOT_SIGNED.toUpperCase(), {
 			width: 585,
 			align: "left",
 		});
@@ -572,14 +612,26 @@ module.exports.pdf = async (req, res, next) => {
 		);
 
 		doc.fontSize(cellFontSize).text("\n", startX, startY + rowHeight * 31);
-		doc.fontSize(noteFontSize).text(text2 + "\n", {
+		doc.fontSize(noteFontSize).text(THIS_CONTRACT_IS + "\n", {
 			width: bodyWidth,
 			align: "justify",
 		});
-		doc.fontSize(noteFontSize).text("\n" + text3, {
+		doc.fontSize(noteFontSize).text("\n" + TEXT_230_Capcom, {
 			width: bodyWidth,
 			align: "center",
 		});
+
+
+		doc.addPage({
+			margins: {
+				top: 72,
+				bottom: 0,
+				left: 72,
+				right: 72,
+			},
+		});
+
+		doc.fontSize(8).text(termsAndConditions);
 
 		doc.end();
 	} catch (e) {
@@ -589,7 +641,7 @@ module.exports.pdf = async (req, res, next) => {
 };
 
 function drawTab(doc, data, startX, startY, columnWidths, rowHeight) {
-	
+
 
 	let x = startX;
 	let y = startY;
@@ -597,7 +649,7 @@ function drawTab(doc, data, startX, startY, columnWidths, rowHeight) {
 	// Determinar el número de columnas basado en la primera fila de datos
 	const numColumns = data.headers ? data.headers.length : data.rows[0].length;
 
-	
+
 
 	// Dibujar encabezados y líneas horizontales (si hay encabezados)
 	if (data.headers && data.headers.length > 0) {
@@ -609,7 +661,7 @@ function drawTab(doc, data, startX, startY, columnWidths, rowHeight) {
 			x += columnWidths[i];
 		});
 
-	
+
 		// Avanzar la posición vertical para las filas
 		y += rowHeight;
 	}
@@ -625,14 +677,14 @@ function drawTab(doc, data, startX, startY, columnWidths, rowHeight) {
 			x += columnWidths[i];
 		});
 
-	
+
 		y += rowHeight;
 	});
 
 	// Dibujar líneas verticales
 	x = startX;
 	for (let i = 0; i <= numColumns; i++) {
-		
+
 		if (i < numColumns) {
 			x += columnWidths[i];
 		}
